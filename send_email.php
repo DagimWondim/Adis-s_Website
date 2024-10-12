c1efc815b8a58265cc42426e5d336eeea375a4bc
Your Name <dagimwondim25@gmail.com>
2024-10-12 07:32:58 +0300

    Abaigal Cleaning Services

---
+++ send_email.php
@@ -0,0 +1,13 @@
+<!-- <?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $service = $_POST['service'];
+  $location = $_POST['location'];
+
+  $to = 'dagimwondim25@gmail.com';
+  $subject = 'New Message from Cleaning Service Form';
+  $message = "Name: $name\nEmail: $email\nPhone: $phone\nService: $service\nLocation: $location";
+
+  mail($to, $subject, $message);
+?> -->
